97f42d34e77ec7e216f5935052f182e3f8b38159
wyleu <wyleus@gmail.com>
2020-03-19 20:03:57 +0000

    Back to basics with 4 encoders

--- ledshow/encoder.py
+++ ledshow/encoder.py
@@ -1,12 +1,15 @@
 """
 Collection of reactions to encoder events.
 """
-from Source import i2cEncoderLibV2
+import RPi.GPIO as GPIO
+import logging
 
-class Encoder():
-    def __init__(self, bus, port):
+from Source import i2cEncoderLibV2 as i2c
 
-        self.encoder = i2cEncoderLibV2.i2cEncoderLibV2(bus, 0x41)
+class Encoder(i2c.i2cEncoderLibV2):
+    def __init__(self, bus, add):
+        self.setup_gpio()
+        self.encoder = i2c.i2cEncoderLibV2(bus, add)
 
     def encoderChange(self):
         self.encoder.writeLEDG(100)
@@ -34,3 +37,42 @@ class Encoder():
         self.encoder.writeLEDR(100)
         print ('Encoder min!')
         self.encoder.writeLEDR(0)
+
+    def setup_gpio(self):
+        GPIO.setmode(GPIO.BCM)
+        self.INT_pin = 4
+        GPIO.setup(self.INT_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def add_event_gpio(self):
+        def enc_int(self):
+            self.encoder.updateStatus()
+        GPIO.add_event_detect(self.INT_pin, GPIO.FALLING, callback=enc_int, bouncetime=10)
+
+    def i2c_setup(self):
+        encconfig = (
+                i2c.INT_DATA | i2c.WRAP_ENABLE | i2c.DIRE_RIGHT | i2c.IPUP_ENABLE | i2c.RMOD_X1 | i2c.RGB_ENCODER)
+        self.encoder.begin(encconfig)
+
+        self.encoder.writeCounter(0)
+        self.encoder.writeMax(35)
+        self.encoder.writeMin(-20)
+        self.encoder.writeStep(1)
+        self.encoder.writeAntibouncingPeriod(8)
+        self.encoder.writeDoublePushPeriod(50)
+        self.encoder.writeGammaRLED(i2c.GAMMA_2)
+        self.encoder.writeGammaGLED(i2c.GAMMA_2)
+        self.encoder.writeGammaBLED(i2c.GAMMA_2)
+
+        self.encoder.onChange = self.encoderChange
+        self.encoder.onButtonPush = self.encoderPush
+        self.encoder.onButtonDoublePush = self.encoderDoublePush
+        self.encoder.onMax = self.encoderMax
+        self.encoder.onMin = self.encoderMin
+
+        self.encoder.autoconfigInterrupt()
+        print('Board ID code: 0x%X' % (self.encoder.readIDCode()))
+        logging.info('Board ID code: 0x%X' % (self.encoder.readIDCode()))
+        print('Board Version: 0x%X' % (self.encoder.readVersion()))
+        logging.info('Board Version: 0x%X' % (self.encoder.readVersion()))
+        logging.error('Board Version: 0x%X' % (self.encoder.readVersion()))
+
--- ledshow/i2c_disp.py
+++ ledshow/i2c_disp.py
@@ -1,11 +1,11 @@
 import smbus2
-import RPi.GPIO as GPIO
 
+
+import RPi.GPIO as GPIO
 import logging
 from time import sleep
 import Source.i2cEncoderLibV2 as i2c
 
-from encoder import Encoder
 
 try:
     logging.info("i2c_disp started cleanly...")
@@ -13,38 +13,58 @@ except Exception as e:
     logging.error("ERROR initializing i2c_disp: %s" % e)
 
 
-def main(reactor):
+if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
+    bus = smbus2.SMBus(1)
     INT_pin = 4
     GPIO.setup(INT_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    encoder = reactor.encoder
 
-    def enc_int(self):
-        encoder.updateStatus()
+    encoder = i2c.i2cEncoderLibV2(bus, 0x41)
+    encoder2 = i2c.i2cEncoderLibV2(bus, 0x42)
+    encoder3 = i2c.i2cEncoderLibV2(bus, 0x43)
+    encoder4 = i2c.i2cEncoderLibV2(bus, 0x44)
+
+
+
+    # Simple callback that ist's called when the encoder is rotated and blink the green led #
 
     encconfig = (
                 i2c.INT_DATA | i2c.WRAP_ENABLE | i2c.DIRE_RIGHT | i2c.IPUP_ENABLE | i2c.RMOD_X1 | i2c.RGB_ENCODER)
     encoder.begin(encconfig)
+    encoder2.begin(encconfig)
+    encoder3.begin(encconfig)
+    encoder4.begin(encconfig)
 
-    encoder.writeCounter(0)
-    encoder.writeMax(35)
-    encoder.writeMin(-20)
-    encoder.writeStep(1)
-    encoder.writeAntibouncingPeriod(8)
-    encoder.writeDoublePushPeriod(50)
-    encoder.writeGammaRLED(i2c.GAMMA_2)
-    encoder.writeGammaGLED(i2c.GAMMA_2)
-    encoder.writeGammaBLED(i2c.GAMMA_2)
 
-    encoder.onChange = reactor.encoderChange
-    encoder.onButtonPush = reactor.encoderPush
-    encoder.onButtonDoublePush = reactor.encoderDoublePush
-    encoder.onMax = reactor.encoderMax
-    encoder.onMin = reactor.encoderMin
+    def EncoderChange():
+        encoder.writeLEDG(100)
+        print('Changed: %d' % (encoder.readCounter32()))
+        encoder.writeLEDG(0)
+
+
+    def Encoder_INT(self):
+        encoder.updateStatus()
+
+    def set_encoder(encoder):
+        encoder.writeCounter(0)
+        encoder.writeMax(35)
+        encoder.writeMin(-20)
+        encoder.writeStep(1)
+        encoder.writeAntibouncingPeriod(8)
+        encoder.writeDoublePushPeriod(50)
+        encoder.writeGammaRLED(i2c.GAMMA_2)
+        encoder.writeGammaGLED(i2c.GAMMA_2)
+        encoder.writeGammaBLED(i2c.GAMMA_2)
+
+    set_encoder(encoder)
+
+    encoder.onChange = EncoderChange  # Attach the event to the callback function#
 
     encoder.autoconfigInterrupt()
-    print('Board ID code: 0x%X' % (encoder.readIDCode()))
-    print('Board Version: 0x%X' % (encoder.readVersion()))
+
+    encoder2.writeLEDR(60)
+    encoder3.writeLEDG(60)
+    encoder4.writeLEDB(60)
 
     encoder.writeRGBCode(0x640000)
     sleep(0.3)
@@ -54,20 +74,13 @@ def main(reactor):
     sleep(0.3)
     encoder.writeRGBCode(0x00)
 
-    GPIO.add_event_detect(INT_pin, GPIO.FALLING, callback=enc_int, bouncetime=10)
+    print('Board ID code: 0x%X' % (encoder.readIDCode()))
+    logging.info('Info Board ID code: 0x%X' % (encoder.readIDCode()))
+    print('Board Version: 0x%X' % (encoder.readVersion()))
+    logging.info('Info Board Version: 0x%X' % (encoder.readVersion()))
+    logging.error('Error Board Version: 0x%X' % (encoder.readVersion()))
+
+    GPIO.add_event_detect(INT_pin, GPIO.FALLING, callback=Encoder_INT, bouncetime=10)
 
     while True:
-        #  if GPIO.input(INT_pin) == False: #
-        #     Encoder_INT() #
-        pass
-
-
-if __name__ == '__main__':
-    bus = smbus2.SMBus(1)
-    reactor = Encoder(bus, 0x41)
-
-    main(reactor)
-    while True:
-        #  if GPIO.input(INT_pin) == False: #
-        #     Encoder_INT() #
         pass
